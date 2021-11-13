26b19da2ceb469d5d4dcd720e88b9a9a3920d0e5
Matias Camiletti <matias.camiletti@gmail.com>
2021-11-13 12:26:41 -0300

    Fix module in core.

--- projects/agencycoda/mia-help-core/src/lib/mia-help-core.module.ts
+++ projects/agencycoda/mia-help-core/src/lib/mia-help-core.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
-import { MiaHelpCoreComponent } from './mia-help-core.component';
 
 
 
 @NgModule({
   declarations: [
-    MiaHelpCoreComponent
   ],
   imports: [
   ],
   exports: [
-    MiaHelpCoreComponent
   ]
 })
 export class MiaHelpCoreModule { }
